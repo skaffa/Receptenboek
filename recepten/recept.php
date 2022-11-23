4e42aefaa0b77c4bbb73b84a7afeee1d054e2262
exari <82609830+exaricode@users.noreply.github.com>
2022-11-23 16:08:35 +0100

    add sections

--- recepten/recept.php
+++ recepten/recept.php
@@ -24,48 +24,51 @@
 </header>
 
 <main>
-    <article id="ingredients">
-        <h2> 12 porties</h2>
-        <ul>
-            <li>270 g rollen vers bladerdeeg</li>
-            <li>50 g groene pesto</li>
-            <li>50 g AH Biologisch geraspte belegen kaas</li>
-        </ul>
-    </article>
+    <section id="recipe">
+        <div>
+            <article id="ingredients">
+                <h2> 12 porties</h2>
+                <ul>
+                    <li>270 g rollen vers bladerdeeg</li>
+                    <li>50 g groene pesto</li>
+                    <li>50 g AH Biologisch geraspte belegen kaas</li>
+                </ul>
+            </article>
 
-    <article id="necesarry">
-        <h2>Dit heb je nodig</h2>
-        <ul>
-            <li>bakplaten</li>
-            <li>bakpapier</li>
-            <li>sateprikkers</li>
-        </ul>
-    </article>
-
-    <article id="preperation">
-        <ul>
-            <li>
-                Verwarm de oven voor op 180 °C.
-            </li>
-            <li>
-                Rol het bladerdeeg uit en snijd (voor 12 porties) in de lengte in 12 plakken van 2 cm breed.
-            </li>
-            <li>
-                Vouw zigzaggend een reep op een met bakpapier beklede bakplaat in steeds kleinere vouwen van 
-                onder naar boven tot een kerstboompje. Je hebt in totaal 2 bakplaten nodig of bak ze in delen.
-            </li>
-            <li>
-                Steek de prikker (geweekt in water) vanaf het bovenste puntje door het kerstboompje zonder het 
-                deeg door te prikken. Zorg ervoor dat de prikker aan de bovenkant er niet te veel uitsteekt. 
-                Druk het deeg tegen de prikker aan.
-            </li>
-            <li>
-                Besmeer met de pesto en bestrooi met de kaas. Bak in het midden van de oven in ca. 15 min. gaar.
-            </li>
-        </ul>
-    </article>
-
-    <article>
+            <article id="necessary">
+                <h2>Dit heb je nodig</h2>
+                <ul>
+                    <li>bakplaten</li>
+                    <li>bakpapier</li>
+                    <li>sateprikkers</li>
+                </ul>
+            </article>
+        </div>
+        <article id="preparation">
+            <ul>
+                <li>
+                    Verwarm de oven voor op 180 °C.
+                </li>
+                <li>
+                    Rol het bladerdeeg uit en snijd (voor 12 porties) in de lengte in 12 plakken van 2 cm breed.
+                </li>
+                <li>
+                    Vouw zigzaggend een reep op een met bakpapier beklede bakplaat in steeds kleinere vouwen van 
+                    onder naar boven tot een kerstboompje. Je hebt in totaal 2 bakplaten nodig of bak ze in delen.
+                </li>
+                <li>
+                    Steek de prikker (geweekt in water) vanaf het bovenste puntje door het kerstboompje zonder het 
+                    deeg door te prikken. Zorg ervoor dat de prikker aan de bovenkant er niet te veel uitsteekt. 
+                    Druk het deeg tegen de prikker aan.
+                </li>
+                <li>
+                    Besmeer met de pesto en bestrooi met de kaas. Bak in het midden van de oven in ca. 15 min. gaar.
+                </li>
+            </ul>
+        </article>
+    </section>
+    
+    <article id="nutrition">
         <h2>Voedingswaarden (portie)</h2>
         <ul>
             <li>energie 120 kcal</li>
