d5431f1179d5b2957a4051eb4ef18f21f7117254
Skaffa <delanovw2000@gmail.com>
2022-11-23 20:29:48 +0100

    Update header.php

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,8 +1,8 @@
 <nav>
     <img src="assets/img/logo.svg" width="55rem" alt="logo">
     <div id="nav-buttons">
-        <a href="#">Home</a>|
-        <a href="#">Recepten</a>
+        <a href="/">Home</a>
+        <a href="recepten/recept.php">Recepten</a>
     </div>
     <div class="placeholder"></div>
 </nav>
