f23dc4b10e208b789d23dedf9279e0ebb8022178
Skaffa <delanovw2000@gmail.com>
2022-12-05 11:27:38 +0100

    smulwev

--- Utilities/Smulweb/collector.py
+++ Utilities/Smulweb/collector.py
@@ -1,16 +1,26 @@
 import requests
 import bs4
 
-source = requests.get('https://www.smulweb.nl/recepten').text
+links = []
+page = 2
+while True:
+    # try:
+    Ppage = str(page)
+    print('Fetching page ' + Ppage)
+    source = requests.get('https://www.smulweb.nl/recepten?page=' + Ppage, timeout=10000).text
+    soup = bs4.BeautifulSoup(source, features="html5lib")
+    for link in soup.findAll('a'):
+        link = link.get('href')
+        if not link == None:
+            if True in [char.isdigit() for char in link]:
+                if 'https://www.smulweb.nl/recepten/' in link:
+                    if not link in links:
+                        links.append(link)
+                        print(link)
+                        with open('all_recipes.txt', 'a', encoding='utf-8') as output:
+                            output.write(link + '\n')
+    page += 1
 
-soup = bs4.BeautifulSoup(source, features="html5lib")
-
-print(source)
-# searchPagination
-pageContent = soup.find("div", {"id": "searchPagination"})
-
-# pageContent = pageContent.findChildren("a", recursive=True)
-pageContent = pageContent.findChildren("article", recursive=True)
-
-for page in pageContent:
-    print(page.text)
+    # except Exception as e:
+    #     print(e)
+    #     continue
