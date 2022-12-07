79f53cbeab737c74b13624a34a57ba25fb568531
exari <82609830+exaricode@users.noreply.github.com>
2022-12-07 16:27:33 +0100

    style

--- Views/BrowseRecipes.php
+++ Views/BrowseRecipes.php
@@ -9,7 +9,7 @@ require_once(__DIR__ . "/../Controllers/RecipeController.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/css/browse.css" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/header.css" />
+    <link rel="stylesheet" href="../assets/css/header.css" />
     <title>Document</title>
 </head>
 <body>
